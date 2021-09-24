30dd722c4238aea8f3b572fc14379624d4e210da
Markus Heizenreder-Reitz <markus.heizenreder-reitz@schulportal.hessen.de>
2021-09-24 15:17:22 +0200

    Rewrite von createSectionHeading zur Verbesserung der Lesbarkeit. Korrektur: Umschliessendes Element für einen Link ist ein span-Tag, NICHT ein div-Tag.

--- classes/HTML.php
+++ classes/HTML.php
@@ -25,7 +25,7 @@ class HTML
     public static function createLinkInNewTab(string $url, string $description): string
     {
         return html_writer::tag(
-            'div',
+            'span',
             html_writer::link($url, $description, ['target' => '_blank'])
         );
     }
@@ -44,25 +44,19 @@ class HTML
 
     public static function createSectionHeading($sectionInfo, $course, $sectionCounter): string
     {
+        $description = $sectionInfo->name;
+        if (is_null($description) || $description === '') {
+            $formatsectionname = get_string_manager()->string_exists('sectionname', 'format_' . $course->format) ? get_string('sectionname', 'format_' . $course->format) : '';
+
+            $description = $formatsectionname . ' ' . $sectionCounter;
+        }
+
         $courseInfo = get_fast_modinfo($course);
-        $formatsectionname = '';
 
-        if (get_string_manager()->string_exists('sectionname', 'format_' . $course->format)) {
-            $formatsectionname = get_string('sectionname', 'format_' . $course->format);
-        }
+        $url = (new moodle_url('/course/view.php', ['id' => $courseInfo->courseid])) . '#section-' . $sectionCounter;
 
-        $url = (new moodle_url('/course/view.php', array('id' => $courseInfo->courseid))) . '#section-' . $sectionCounter;
-
-        $sectionname = $sectionInfo->name;
-        $anzuzeigenderText = '';
-        if (is_null($sectionname) || $sectionname === '') {
-            $anzuzeigenderText = $formatsectionname . ' ' . $sectionCounter;
-        } else {
-            $anzuzeigenderText = $sectionname;
-        }
-
-        $linktext = html_writer::link($url, $anzuzeigenderText);
-        $linktext2 = html_writer::link($url, '📑', ['target' => '_blank']);
+        $linktext = html_writer::link($url, $description);
+        $linktext2 = HTML::createLinkInNewTab($url, '📑');
 
         return html_writer::tag('h3', '(' . $sectionCounter . ') ' . $linktext . ' ' .  $linktext2, ['class' => 'orphandfilesh3']);
     }
