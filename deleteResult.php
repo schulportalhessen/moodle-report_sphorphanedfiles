a9998cf071281031116d4c203aaa4802d8a1de4d
Markus Heizenreder-Reitz <markus.heizenreder-reitz@schulportal.hessen.de>
2021-09-24 20:17:27 +0200

    Funktionalität zur Verwaltung von Listen von FileInfo-Objekten realisiert. Diese bilden die technische Grundlage für die Umsetzung der Auswahl von mehr als einem Löschobjekt. Demonstration der Funktionalität in deleteResult.php umgesetzt.

--- deleteResult.php
+++ deleteResult.php
@@ -1,18 +1,28 @@
 <?php
 
+require_once(__DIR__ . '/../../config.php');
+
+use report_sphorphanedfiles\Files\FileInfoList;
+
 echo '<html>';
 echo '<body>';
 
-$items = $_POST['selectedItems'];
+if (FileInfoList::isSufficientForConstruction($_POST))
+ {
+    $myList = new FileInfoList($_POST);
 
-if (!empty($items)) {
-    echo "<h1>Die nachfolgenden Dateien sind für das Löschen ausgewählt</h1>";
-    echo '<ul>';
-    foreach ($items as $item)
-        echo "<li>Zu löschen: " . $item . "</li>";
-    echo '</ul>';
+    if (!$myList->isEmpty()) {
+        echo "<h1>Die nachfolgenden Dateien sind für das Löschen ausgewählt</h1>";
+
+        echo $myList->toHTML();
+
+        echo '<b>Der Aufruf $myList->delete(...) würde die obigen Dateien löschen</b>';
+    } else {
+        echo "<b>Keine Objekte ausgewählt!</b>";
+    }
 } else {
-    echo "<b>Keine Objekte ausgewählt!</b>";
+    echo "<b>FATAL: Keine FileInfo Objekte vorhanden!</b>";
 }
+
 echo '</body>';
 echo '</html>';
