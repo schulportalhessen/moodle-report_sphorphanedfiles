31b44c32784df9146febe0155e07096d7eb5851c
Andreas Schenkel <Andreas.Schenkel@schulportal.hessen.de>
2021-11-12 08:15:17 +0100

    autor

--- version.php
+++ version.php
@@ -17,7 +17,6 @@
 /**
  * @package     report_sphorphanedfiles
  * @copyright   Schulportal Hessen (SPH)
- * @author      Stefan Buchhofer
  * @author      Andreas Schenkel <andreas.schenkel@schulportal.hessen.de>
  * @author      Markus Heizenreder-Reitz
  * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
@@ -27,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'report_sphorphanedfiles';
 $plugin->release = '1.0.0';
-$plugin->version = 2021110800;
+$plugin->version = 2021110801;
 $plugin->requires = 2020061500;
 $plugin->maturity = MATURITY_ALPHA;
