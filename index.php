7ee1705246530d356fc90f46d6abac9bb57a4673
Andreas Schenkel <Andreas.Schenkel@schulportal.hessen.de>
2022-07-27 13:00:28 +0200

    add require_login and delete only after check if report is active

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
-
 require_once(__DIR__ . '/../../config.php');
-
 use report_sphorphanedfiles\View\OrphanedView;
 
 /* Assign global variables to local (parameter) variables.
@@ -11,27 +9,30 @@ use report_sphorphanedfiles\View\OrphanedView;
  * FIXME: Move to method call. 
  */
 
+require_login();
 $courseId = required_param('id', PARAM_INT);
+require_login($courseId);
+
+
 $page = $PAGE;
 $output = $OUTPUT;
 $user = $USER;
 $db = $DB;
 
-$orphanedViewInstance = new OrphanedView($db, $courseId, $page, $output, $user);
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $orphanedViewInstance->deleteOrphanedFile();
-}
-
 global $CFG;
+echo "Hurz";
 
 $isactive = $CFG->report_sphorphanedfiles_isactive;
 $isactiveforadmin = $CFG->report_sphorphanedfiles_isactiveforadmin;
-
 // $hascapability = has_capability('report/sphorphanedfiles:view',$context);
 
 if ($isactive || $isactiveforadmin) {
+    $orphanedViewInstance = new OrphanedView($db, $courseId, $page, $output, $user);
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $orphanedViewInstance->deleteOrphanedFile();
+    }
     $orphanedViewInstance->init($isactive, $isactiveforadmin);
 } else {
     echo "Report is not activated or missing capability";
 }
+
